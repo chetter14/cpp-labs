a9b144f6fb05cd434db317b1b28728db20911ef3
Artem Leshchukov <artemleshchukov02@gmail.com>
2024-09-27 16:28:27 +0300

    Wrote the algorithm of the 1st lab

---
+++ lab1/main.cpp
@@ -0,0 +1,7 @@
+/*
+Algorithm:
+	1) Read variables types from console (no check, assume the type entered is correct)
+	2) Print out variables initialization into the file stream
+	3) Read operators from some file input. Separetely, at each line (i.e., "*\r\n+\r\n/\r\n-\r\n")
+	4) Print out the operator and operands into the file stream
+*/
